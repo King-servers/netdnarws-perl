3dcc596cf939343fb994b1802ce556d2d9b55f22
sindbad911 <sindbad911@bk.ru>
2014-05-31 14:47:38 +0300

    Initial commit

--- sample.pl
+++ sample.pl
@@ -11,17 +11,3 @@ $api = new NetDNARWS( 'bastosventures', '8eead644a4614b99bcd', 'ab81cf43397eb0e'
 $api->get("/account.json");
 
 $api->get("/account.json", 1);
-
-# Get first name which is set using constructor.
-#$alias = $api->getAlias();
-
-#print "Before Setting Alias is : $alias\n";
-
-# Now Set alias using function.
-#$api->setAlias( "bastosventures" );
-
-# Now get alias set by function.
-#$alias = $api->getAlias();
-#print "After Setting Alias is : $alias\n";
-
-
